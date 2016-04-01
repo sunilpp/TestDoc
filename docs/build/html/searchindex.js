453d56297339cb009c99d61351b29699bb38a2b3
Sunil Padmanabhan <psunil@Sunils-MacBook-Pro.local>
2016-03-31 19:08:52 -0700

    initial commit

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Enterprise Build and Release  documentation!"],titleterms:{build:0,doctut:[],document:0,enterpris:0,indic:0,releas:0,tabl:0,welcom:0}})
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+Welcome to Enterprise Build and Release  documentation!
+=======================================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
